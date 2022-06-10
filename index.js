1b835ea808d4af68cfae45a5d33f0a6642dc4d07
Acik <kodgiroartur228@gmail.com>
2022-06-10 18:31:26 +0300

    Fixed game

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const secretNumber = "2109";
+const secretNumber = 2109;
 let TRY = 1;
 
 while (true) {
   const number = prompt("Guess secret number:");
-  if (number === secretNumber) {
+  if (number === "" || number === null || isNaN(Number(number)) || number < 0) {
+    console.log("Error! Enter correct number.");
+  } else if (Number(number) === secretNumber) {
     console.log("You guessed the secret number!");
     console.log("Tries: " + TRY);
     break;
-  }
-  if(number < secretNumber){
+  } else if (Number(number) < secretNumber) {
     TRY++;
-    console.log('Secret number is more than entered number!')
+    console.log("Secret number is more than entered number!");
     console.log("Try again:");
-    continue
-  }
-  if(number > secretNumber){
+    continue;
+  } else if (Number(number) > secretNumber) {
     TRY++;
-    console.log('Secret number is less than entered number!')
+    console.log("Secret number is less than entered number!");
     console.log("Try again:");
-    continue
+    continue;
   }
 }
