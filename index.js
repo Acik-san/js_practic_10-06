401483d728e813e04c79fcb857558c7160d6d2b9
Acik <kodgiroartur228@gmail.com>
2022-06-10 18:10:15 +0300

    Add game"Guess the number"

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const secretNumber = "2109";
+let TRY = 1;
 
+while (true) {
+  const number = prompt("Guess secret number:");
+  if (number === secretNumber) {
+    console.log("You guessed the secret number!");
+    console.log("Tries: " + TRY);
+    break;
+  }
+  if(number < secretNumber){
+    TRY++;
+    console.log('Secret number is more than entered number!')
+    console.log("Try again:");
+    continue
+  }
+  if(number > secretNumber){
+    TRY++;
+    console.log('Secret number is less than entered number!')
+    console.log("Try again:");
+    continue
+  }
+}
